3cf14eed19de6cc5e27d864b226ee9dfdf569e77
SubhanWasif <rustzee93@gmail.com>
2024-03-19 13:07:24 +0500

    Updates

--- VideoFrames.py
+++ VideoFrames.py
@@ -6,33 +6,58 @@ import os
 from pytube import YouTube
 from pymongo import MongoClient
 import cv2
+from dotenv import load_dotenv
 
 
+load_dotenv()
 
-# Connecting to the MongoDB
-client = MongoClient('mongodb://localhost:27017/')
+# Retrieve environment variables
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")  # Assuming it's already URL-encoded
+cluster_address = os.getenv("MONGO_CLUSTER_ADDRESS")
+database = os.getenv("MONGO_DATABASE")  # Assuming 'test' is your database name
+options = "retryWrites=true&w=majority"  # Your connection options
+
+# Construct the MongoDB URI
+mongo_uri = f"mongodb+srv://{username}:{password}@{cluster_address}/{database}?{options}"
+
+# Initialize the MongoDB client
+client = MongoClient(mongo_uri)
 db = client['Database']
 collection = db['FrameCollection']
+print("Connection is Successfull!")
 
 def download_youtube_video(url, path):
-    yt = YouTube(url)
-    stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-    video_id = yt.video_id
-    stream.download(output_path=path, filename='video.mp4')
-    print('Video downloaded successfully.')
-    return video_id
+    attempts = 0
+    max_attempts = 5  # Maximum number of attempts
+    while attempts < max_attempts:
+        try:
+            yt = YouTube(url)
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            video_id = yt.video_id
+            stream.download(output_path=path, filename='video.mp4')
+            print('Video downloaded successfully.')
+            return video_id
+        except requests.exceptions.RequestException as e:
+            print(f"Attempt {attempts + 1} failed with IncompleteRead: {e}")
+            attempts += 1
+            if attempts == max_attempts:
+                raise  # Reraise the last exception if all attempts fail
+
 
 def extract_frames(video_path, frame_rate):
     vidcap = cv2.VideoCapture(video_path)
     success, image = vidcap.read()
     count = 0
     extracted_frames = []
+    fps = vidcap.get(cv2.CAP_PROP_FPS)  # Get the frames per second of the video
+    
     while success:
         if count % frame_rate == 0:
-            frame_path = f"frame_{count}.jpg"
-            cv2.imwrite(frame_path, image)
-            extracted_frames.append(frame_path)
-            print(f"Extracted frame {count}")
+            frame_path = f"frame_{count/fps:.2f}.jpg"
+            cv2.imwrite(frame_path, image)  # Save frame as JPEG file
+            extracted_frames.append((frame_path, count / fps))  # Append frame path and timestamp
+            print(f"Extracted frame {count} at {count/fps:.2f} seconds")
         success, image = vidcap.read()
         count += 1
     return extracted_frames
@@ -41,7 +66,7 @@ async def encode_frame_to_base64(frame_path):
     with open(frame_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-async def send_frame_to_api(frame, video_id, api_endpoint):
+async def send_frame_to_api(frame,time, video_id, api_endpoint):
     frame_base64 = await encode_frame_to_base64(frame)  # await the async function
     payload = {
         'frameData': "data:image/jpeg;base64," + frame_base64,
@@ -54,6 +79,8 @@ async def send_frame_to_api(frame, video_id, api_endpoint):
         if response.json() != []:
             data = response.json()
             data = data[0]
+            data['video_id'] = video_id
+            data['timestamp'] = int(time)
             collection.insert_one(data)
             
 
@@ -75,15 +102,16 @@ if __name__ == "__main__":
 
     # Send extracted frames to API
     for frame in extracted_frames:
-        asyncio.run(send_frame_to_api(frame, 'GVPzGBvPrzw', API_ENDPOINT))
+        asyncio.run(send_frame_to_api(frame[0],frame[1], video_id, API_ENDPOINT))
 
     # Cleanup: Remove downloaded video and extracted frames
     os.remove(video_path)
     for frame in extracted_frames:
-        os.remove(frame)
+        os.remove(frame[0])
     for doc in collection.find():
         print(doc)
     client.close()
+
 
 
 
